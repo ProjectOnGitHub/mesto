9cb60782eb0a7abce3929fa05eef51234109cbec
Aleksandr <soft@vhlv.ru>
2020-09-11 21:11:11 +0300

    fix: сделал, чтобы попапы profile и place открывались и закрывались

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,12 @@
 // popup
-// Объявляем переменные
+// Объявляем переменные для попапов
 const popupProfile = document.querySelector('.popup_type-edit-profile');
 const popupPlace = document.querySelector('.popup_type-add-place');
-const popupOpenEditButton = document.querySelector('.profile__edit-button');
-const popupOpenAddButton = document.querySelector('.profile__add-button')
-const popupProfileClose = popupProfile.querySelector('.popup__close-button');
-const popupPlaceClose = popupPlace.querySelector('.popup__close-button');
+//const popup
+const popupProfileOpenButton = document.querySelector('.profile__edit-button');
+const popupPlaceOpenButton = document.querySelector('.profile__add-button')
+const popupProfileCloseButton = popupProfile.querySelector('.popup__close-button');
+const popupPlaceCloseButton = popupPlace.querySelector('.popup__close-button');
 
 // Создаем функцию, которая будет открывать popup Редактировать профиль
 function popupProfileOpen() {
@@ -17,22 +18,27 @@ function popupPlaceOpen() {
   popupPlace.classList.toggle('popup_opened');
 }
 
+// Создаем функцию, которая будет закрывать popup Редактировать профиль
 function popupProfileClose() {
   popupProfile.classList.toggle('popup_opened');
 }
 
+// Создаем функцию, которая будет закрывать popup Новое место
 function popupPlaceClose() {
   popupPlace.classList.toggle('popup_opened');
 }
 
-// Навешиваем обработчики событий на кнопки, которые будут открывать/закрывать popup
-popupOpenEditButton.addEventListener('click', popupProfileOpen);
-popupOpenAddButton.addEventListener('click', popupPlaceOpen);
-popupProfileClose.addEventListener('click', popupProfileClose);
+// Навешиваем обработчики событий на кнопки, которые будут открывать/закрывать popup Редактировать профиль
+popupProfileOpenButton.addEventListener('click', popupProfileOpen);
+popupProfileCloseButton.addEventListener('click', popupProfileClose);
 
-// Форма редактирования
+// Навешиваем обработчики событий на кнопки, которые будут открывать/закрывать popup Новое место
+popupPlaceOpenButton.addEventListener('click', popupPlaceOpen);
+popupPlaceCloseButton.addEventListener('click', popupPlaceClose);
+
+// Форма Редактировать профиль
 // Объявляем переменные
-let formElement = document.querySelector('.popup__container');
+let formElement = popupProfile.querySelector('.popup__container');
 let nameInput = document.querySelector('.popup__input_name');
 let jobInput = document.querySelector('.popup__input_job');
 let profileName = document.querySelector('.profile__title');
@@ -50,7 +56,7 @@ function formSubmitHandler (evt) {
     // Получите значение полей из свойства value для profile
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
-    popupClose();
+    popupProfileClose();
   }
 
 // Прикрепляем обработчик к форме:
