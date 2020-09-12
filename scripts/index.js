bc65a19fdd29093d0030cd3325538c3bc00503b2
Aleksandr <soft@vhlv.ru>
2020-09-12 16:50:10 +0300

    fit: сделал лайки

--- scripts/index.js
+++ scripts/index.js
@@ -1,93 +1,86 @@
-
-// Объявляем переменные для попапов
+// Попап Редактировать профиль //
+// Объявляем переменные для попапа Редактировать профиль
 const popupProfile = document.querySelector('.popup_type-edit-profile');
-const popupPlace = document.querySelector('.popup_type-add-place');
-const popupImage = document.querySelector('.popup_type-view-image');
-
-//Объявляем переменные для форм
 const formProfile = popupProfile.querySelector('.popup__container');
-const formPlace = popupPlace.querySelector('.popup__container');
-
-//Объявляем переменные для полей форм
-let nameInput = formProfile.querySelector('.popup__input_name');
-let jobInput = formProfile.querySelector('.popup__input_job');
-//let placeInput = formPlace.querySelector('.popup__input_place');
-//let urlInput = formPlace.querySelector('.popup__input_url');
-
-//Объявляем переменные для кнопок
 const popupProfileOpenButton = document.querySelector('.profile__edit-button');
 const popupProfileCloseButton = popupProfile.querySelector('.popup__close-button');
-const popupPlaceOpenButton = document.querySelector('.profile__add-button')
-const popupPlaceCloseButton = popupPlace.querySelector('.popup__close-button');
-const popupImageOpenButton = document.querySelectorAll('.cards__image');
-const popupImageCloseButton = popupImage.querySelector('.popup__close-button');
-// Создаем функцию для открытия попапа изображение
-popupImageOpenButton.forEach(item => item.addEventListener('click', function(){
-  popupImage.classList.toggle('popup_opened');
-}))
+let nameInput = formProfile.querySelector('.popup__input_name');
+let jobInput = formProfile.querySelector('.popup__input_job');
+let profileName = document.querySelector('.profile__title');
+let profileJob = document.querySelector('.profile__subtitle');
 
-// Создаем функцию, которая будет открывать popup Редактировать профиль
+// Создаем функции, которые будут открывать/закрывать попап Редактировать профиль
 function popupProfileOpen() {
   popupProfile.classList.toggle('popup_opened');
 }
-
-// Создаем функцию, которая будет закрывать popup Редактировать профиль
 function popupProfileClose() {
   popupProfile.classList.toggle('popup_opened');
 }
-
-// Создаем функцию, которая будет открывать popup Новое место
-function popupPlaceOpen() {
-  popupPlace.classList.toggle('popup_opened');
-}
-
-// Создаем функцию, которая будет закрывать popup Новое место
-function popupPlaceClose() {
-  popupPlace.classList.toggle('popup_opened');
-}
-
-// Создаем функцию, которая будет открывать popup изображение
-//function popupImageOpen() {
-//  popupImage.classList.toggle('popup_opened');
-//}
-
-// Создаем функцию, которая будет закрывать popup изображение
-function popupImageClose() {
-  popupImage.classList.toggle('popup_opened');
-}
-
-
-
-// Навешиваем обработчики событий на кнопки, которые будут открывать/закрывать popup Редактировать профиль
+// Навешиваем обработчики событий на кнопки, которые будут открывать/закрывать попап Редактировать профиль
 popupProfileOpenButton.addEventListener('click', popupProfileOpen);
 popupProfileCloseButton.addEventListener('click', popupProfileClose);
-
-// Навешиваем обработчики событий на кнопки, которые будут открывать/закрывать popup Новое место
-popupPlaceOpenButton.addEventListener('click', popupPlaceOpen);
-popupPlaceCloseButton.addEventListener('click', popupPlaceClose);
-
-// Создаем переменные для получения значений и профиля
-let profileName = document.querySelector('.profile__title');
-let profileJob = document.querySelector('.profile__subtitle');
 
 // Получаем значения для полей формы из profile
 nameInput.value = profileName.textContent;
 jobInput.value = profileJob.textContent;
 
-// Создаем функцию, которая сохраняет значения popup Редактировать профиль
+// Создаем функцию, которая сохраняет значения попап Редактировать профиль
 function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                        // Так мы можем определить свою логику отправки.
-                        // О том, как это делать, расскажем позже.
-    // Получаем значение полей из свойства value для profile
-    profileName.textContent = nameInput.value;
-    profileJob.textContent = jobInput.value;
-    popupProfileClose();
-  }
+  evt.preventDefault();
+  // Получаем значение полей из свойства value для profile
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  popupProfileClose();
+}
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formProfile.addEventListener('submit', formSubmitHandler);
 
-// Навешиваем обработчики событий на кнопки, которые будут открывать/закрывать popup Изображение
+
+// Попап Новое место //
+// Объявляем переменные для попапа Новое место
+const popupPlace = document.querySelector('.popup_type-add-place');
+const formPlace = popupPlace.querySelector('.popup__container');
+const popupPlaceOpenButton = document.querySelector('.profile__add-button')
+const popupPlaceCloseButton = popupPlace.querySelector('.popup__close-button');
+//let placeInput = formPlace.querySelector('.popup__input_place');
+//let urlInput = formPlace.querySelector('.popup__input_url');
+// Создаем функции, которые будут открывать/закрывать popup Новое место
+function popupPlaceOpen() {
+  popupPlace.classList.toggle('popup_opened');
+}
+function popupPlaceClose() {
+  popupPlace.classList.toggle('popup_opened');
+}
+// Навешиваем обработчики событий на кнопки, которые будут открывать/закрывать popup Новое место
+popupPlaceOpenButton.addEventListener('click', popupPlaceOpen);
+popupPlaceCloseButton.addEventListener('click', popupPlaceClose);
+
+
+// Попап Изображение //
+// Объявляем переменные для попапа Изображение
+const popupImage = document.querySelector('.popup_type-view-image');
+const popupImageOpenButton = document.querySelectorAll('.cards__image');
+const popupImageCloseButton = popupImage.querySelector('.popup__close-button');
+
+// Создаем функцию для открытия попапа изображение
+popupImageOpenButton.forEach(item => item.addEventListener('click', function(){
+  popupImage.classList.toggle('popup_opened');
+}))
+
+// Создаем функцию, которая будет закрывать попап Изображение
+function popupImageClose() {
+  popupImage.classList.toggle('popup_opened');
+}
+
+// Навешиваем обработчики событий на кнопки, которые будут закрывать попап Изображение
 popupImageCloseButton.addEventListener('click', popupImageClose);
+
+
+// Лайк
+const cardLike = document.querySelectorAll('.cards__like');
+// Создаем функцию для открытия попапа изображение
+cardLike.forEach(item => item.addEventListener('click', function(){
+  item.classList.toggle('cards__like_active');
+}))
