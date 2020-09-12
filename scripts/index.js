0402f284603eb41c39ad155d5efdaf70beca43f5
Aleksandr <soft@vhlv.ru>
2020-09-12 10:06:57 +0300

    fit: правки index js

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,35 @@
-// popup
+
 // Объявляем переменные для попапов
 const popupProfile = document.querySelector('.popup_type-edit-profile');
+const popupPlace = document.querySelector('.popup_type-add-place');
+const popupImage = document.querySelector('.popup_type-view-image');
+
+//Объявляем переменные для форм
+const formProfile = popupProfile.querySelector('.popup__container');
+const formPlace = popupPlace.querySelector('.popup__container');
+
+//Объявляем переменные для полей форм
+const nameInput = popupProfile.querySelector('.popup__input_name');
+const jobInput = popupProfile.querySelector('.popup__input_job');
+const placeInput = popupPlace.querySelector('.popup__input_place');
+const urlInput = popupPlace.querySelector('.popup__input_url');
+
+//Объявляем переменные для кнопок
 const popupProfileOpenButton = document.querySelector('.profile__edit-button');
 const popupProfileCloseButton = popupProfile.querySelector('.popup__close-button');
-const popupPlace = document.querySelector('.popup_type-add-place');
 const popupPlaceOpenButton = document.querySelector('.profile__add-button')
 const popupPlaceCloseButton = popupPlace.querySelector('.popup__close-button');
-const popupImage = document.querySelector('.popup_type-view-image');
 const popupImageOpenButton = document.querySelectorAll('.cards__image');
 const popupImageCloseButton = popupImage.querySelector('.popup__close-button');
+
+let profileName = document.querySelector('.profile__title');
+let profileJob = document.querySelector('.profile__subtitle');
+
+
+
+
+
+
 
 // Создаем функцию, которая будет открывать popup Редактировать профиль
 function popupProfileOpen() {
@@ -51,14 +72,10 @@ popupPlaceCloseButton.addEventListener('click', popupPlaceClose);
 // Навешиваем обработчики событий на кнопки, которые будут открывать/закрывать popup Изображение
 popupImageOpenButton.addEventListener('click', popupImageOpen);
 popupImageCloseButton.addEventListener('click', popupImageClose);
-console.log(popupImageOpen);
+
 // Форма Редактировать профиль
 // Объявляем переменные
-let formElement = popupProfile.querySelector('.popup__container');
-let nameInput = document.querySelector('.popup__input_name');
-let jobInput = document.querySelector('.popup__input_job');
-let profileName = document.querySelector('.profile__title');
-let profileJob = document.querySelector('.profile__subtitle');
+
 
 // Получаем значения для полей формы из profile
 nameInput.value = profileName.textContent;
@@ -77,4 +94,4 @@ function formSubmitHandler (evt) {
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', formSubmitHandler);
+formProfile.addEventListener('submit', formSubmitHandler);
