7edbeaec573324819bf51b8ffa2b4b28dfcf617e
Aleksandr <soft@vhlv.ru>
2020-09-14 00:48:37 +0300

    fit: карточки подтягиваются из массива

--- scripts/index.js
+++ scripts/index.js
@@ -44,8 +44,6 @@ const popupPlace = document.querySelector('.popup_type-add-place');
 const formPlace = popupPlace.querySelector('.popup__container');
 const popupPlaceOpenButton = document.querySelector('.profile__add-button')
 const popupPlaceCloseButton = popupPlace.querySelector('.popup__close-button');
-//let placeInput = formPlace.querySelector('.popup__input_place');
-//let urlInput = formPlace.querySelector('.popup__input_url');
 // Создаем функции, которые будут открывать/закрывать popup Новое место
 function popupPlaceOpen() {
   popupPlace.classList.toggle('popup_opened');
@@ -58,12 +56,74 @@ popupPlaceOpenButton.addEventListener('click', popupPlaceOpen);
 popupPlaceCloseButton.addEventListener('click', popupPlaceClose);
 
 
+
+
+
+
+const initialCards = [
+  {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const cardTemplate = document.querySelector('.cards__template').content;
+const cards = document.querySelector('.cards__list');
+const newCard = cardTemplate.cloneNode(true);
+const cardDeleteButton = newCard.querySelector('.cards__delete-button');
+const placeInput = formPlace.querySelector('.popup__input_place');
+const urlInput = formPlace.querySelector('.popup__submit-button');
+
+function render() {
+  initialCards.forEach(renderItem);
+}
+
+function renderItem({link, name}) {
+  const newCard = cardTemplate.cloneNode(true);
+  newCard.querySelector('.cards__title').innerText = name;
+  newCard.querySelector('.cards__image').src = link;
+  newCard.querySelector('.cards__image').alt = name;
+  cards.appendChild(newCard);
+
+}
+
+render();
+
+
+const cardLike = document.querySelectorAll('.cards__like');
+// Создаем функцию для открытия попапа изображение
+cardLike.forEach(item => item.addEventListener('click', function(){
+  item.classList.toggle('cards__like_active');
+}))
+
+
+
+
 // Попап Изображение //
 // Объявляем переменные для попапа Изображение
 const popupImage = document.querySelector('.popup_type-view-image');
 const popupImageOpenButton = document.querySelectorAll('.cards__image');
 const popupImageCloseButton = popupImage.querySelector('.popup__close-button');
-
 // Создаем функцию для открытия попапа изображение
 popupImageOpenButton.forEach(item => item.addEventListener('click', function(){
   popupImage.classList.toggle('popup_opened');
@@ -76,11 +136,3 @@ function popupImageClose() {
 
 // Навешиваем обработчики событий на кнопки, которые будут закрывать попап Изображение
 popupImageCloseButton.addEventListener('click', popupImageClose);
-
-
-// Лайк
-const cardLike = document.querySelectorAll('.cards__like');
-// Создаем функцию для открытия попапа изображение
-cardLike.forEach(item => item.addEventListener('click', function(){
-  item.classList.toggle('cards__like_active');
-}))
