be7ad4a26dea44efae3681685a1ab32637ff1ff4
Aleksandr <soft@vhlv.ru>
2020-10-14 00:20:10 +0300

    ref: вынес глобальные переменные в index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,8 @@
+import Card from './Card.js';
+import FormValidator from './FormValidator.js';
+
+// Глобальные переменные
+// Попап Профиль
 const popupProfile = document.querySelector('.popup_type-edit-profile');
 const formProfile = popupProfile.querySelector('.popup__container');
 const popupProfileOpenButton = document.querySelector('.profile__edit-button');
@@ -6,6 +11,54 @@ const nameInput = formProfile.querySelector('.popup__input_name');
 const jobInput = formProfile.querySelector('.popup__input_job');
 const profileName = document.querySelector('.profile__title');
 const profileJob = document.querySelector('.profile__subtitle');
+
+// Попап Новое место
+const popupPlace = document.querySelector('.popup_type-add-place');
+const formPlace = popupPlace.querySelector('.popup__container');
+const popupPlaceOpenButton = document.querySelector('.profile__add-button')
+const popupPlaceCloseButton = popupPlace.querySelector('.popup__close-button');
+const cardTemplate = document.querySelector('.cards__template').content;
+const cards = document.querySelector('.cards__list');
+const cardsItem = document.querySelector('.cards__list-item');
+const placeInput = formPlace.querySelector('.popup__input_place');
+const urlInput = formPlace.querySelector('.popup__input_url');
+
+// Попап Фото
+const popupPhoto = document.querySelector('.popup_type-view-image');
+const popupPhotoOpenButton = document.querySelectorAll('.cards__image');
+const popupPhotoTitle = document.querySelectorAll('.cards__title');
+const popupPhotoCloseButton = popupPhoto.querySelector('.popup__close-button');
+const popupImage = popupPhoto.querySelector('.popup__image');
+const popupCaption = popupPhoto.querySelector('.popup__caption');
+
+const initialCards = [
+  {
+    name: 'Андромеда',
+    link: 'https://images.unsplash.com/photo-1543722530-d2c3201371e7'
+  },
+  {
+    name: 'Луна',
+    link: 'https://images.unsplash.com/photo-1509647648544-a3e09b751ad6'
+  },
+  {
+    name: 'Сомбреро',
+    link: 'https://images.unsplash.com/photo-1560740583-0664e57560e4'
+  },
+  {
+    name: 'Земля',
+    link: 'https://images.unsplash.com/photo-1451187580459-43490279c0fa'
+  },
+  {
+    name: 'Млечный путь',
+    link: 'https://images.unsplash.com/photo-1531306728370-e2ebd9d7bb99'
+  },
+  {
+    name: 'Солнце',
+    link: 'https://images.unsplash.com/photo-1575881875475-31023242e3f9'
+  }
+];
+
+
 
 function openPopup(popup) {
   popup.classList.add('popup_opened');
@@ -20,13 +73,13 @@ function closePopup(popup) {
 function closePopupEscape(evt) {
   const popupOpened = document.querySelector('.popup_opened');
   if (evt.key === "Escape" && popupOpened) {
-  closePopup(popupOpened);
+    closePopup(popupOpened);
   }
 }
 
 function closePopupOverlay(event) {
-  if(event.target.classList.contains('popup')) {
-      closePopup(event.target);
+  if (event.target.classList.contains('popup')) {
+    closePopup(event.target);
   }
 };
 
@@ -48,16 +101,6 @@ function formSubmitProfileHandler(evt) {
 }
 
 formProfile.addEventListener('submit', formSubmitProfileHandler);
-
-const popupPlace = document.querySelector('.popup_type-add-place');
-const formPlace = popupPlace.querySelector('.popup__container');
-const popupPlaceOpenButton = document.querySelector('.profile__add-button')
-const popupPlaceCloseButton = popupPlace.querySelector('.popup__close-button');
-const cardTemplate = document.querySelector('.cards__template').content;
-const cards = document.querySelector('.cards__list');
-const cardsItem = document.querySelector('.cards__list-item');
-const placeInput = formPlace.querySelector('.popup__input_place');
-const urlInput = formPlace.querySelector('.popup__input_url');
 
 function render() {
   initialCards.forEach(renderItem);
@@ -109,13 +152,6 @@ formPlace.addEventListener('submit', (evt) => {
 });
 
 render();
-
-const popupPhoto = document.querySelector('.popup_type-view-image');
-const popupPhotoOpenButton = document.querySelectorAll('.cards__image');
-const popupPhotoTitle = document.querySelectorAll('.cards__title');
-const popupPhotoCloseButton = popupPhoto.querySelector('.popup__close-button');
-const popupImage = popupPhoto.querySelector('.popup__image');
-const popupCaption = popupPhoto.querySelector('.popup__caption');
 
 popupProfileOpenButton.addEventListener('click', () => {
   takeProfileValue();
