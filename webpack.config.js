2a5d6cf9ae5253384bf49e2d5178015e08ea75db
Aleksandr <soft@vhlv.ru>
2020-11-04 23:00:40 +0300

    установил babel

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+    entry: { main: './src/index.js' },
+    output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+    module: {
+    rules: [ // rules — это массив правил
+      // добавим в него объект правил для бабеля
+      {
+        // регулярное выражение, которое ищет все js файлы
+        test: /\.js$/,
+        // при обработке этих файлов нужно использовать babel-loader
+        loader: 'babel-loader',
+        // исключает папку node_modules, файлы в ней обрабатывать не нужно
+        exclude: '/node_modules/'
+      }
+      ]
+  }
+};
