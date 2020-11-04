4df22063f2e0f40e3b48211477084ab909708db6
Aleksandr <soft@vhlv.ru>
2020-11-05 00:12:50 +0300

    настроил вебпак

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,47 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    entry: { main: './src/index.js' },
-    output: {
+  entry: { main: './src/index.js' },
+  output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js'
   },
-    module: {
-    rules: [ // rules — это массив правил
-      // добавим в него объект правил для бабеля
+  module: {
+    rules: [
       {
-        // регулярное выражение, которое ищет все js файлы
         test: /\.js$/,
-        // при обработке этих файлов нужно использовать babel-loader
         loader: 'babel-loader',
-        // исключает папку node_modules, файлы в ней обрабатывать не нужно
         exclude: '/node_modules/'
-      }
-      ]
-  }
+      },
+      {
+        test: /\.css$/,
+        loader: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
+          },
+          'postcss-loader'
+        ],
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.(png|svg|jpg|gif|woff|woff2)$/,
+        loader: 'file-loader',
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new MiniCssExtractPlugin()
+  ]
 };
