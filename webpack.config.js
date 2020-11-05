6c029af4d5349d74851dae7a2e77a7d92a1eeb5f
Aleksandr <soft@vhlv.ru>
2020-11-05 20:21:41 +0300

    настроил сборку вебпак

--- webpack.config.js
+++ webpack.config.js
@@ -3,45 +3,57 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: { main: './src/index.js' },
+  entry: './src/pages/index.js',
   output: {
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js'
+    publicPath: '/',
   },
+
   module: {
     rules: [
       {
         test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: '/node_modules/'
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
       },
       {
-        test: /\.css$/,
-        loader: [
+        test: /\.(png|jpe?g|gif|svg|woff|woff2)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: [
           MiniCssExtractPlugin.loader,
           {
             loader: 'css-loader',
             options: {
-              importLoaders: 1
-            }
+              importLoaders: 1,
+            },
           },
           'postcss-loader'
         ],
       },
-      {
-        test: /\.html$/,
-        loader: 'html-loader',
-      },
-      {
-        test: /\.(png|svg|jpg|gif|woff|woff2)$/,
-        loader: 'file-loader',
-      },
     ]
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'src/index.html'
-    }),
-    new MiniCssExtractPlugin()
-  ]
+  plugins: [new HtmlWebpackPlugin({
+    template: './src/index.html'
+  }),
+  new MiniCssExtractPlugin()
+  ],
+
+  devtool: 'inline-source-map',
 };
